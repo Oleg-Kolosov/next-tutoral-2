44c172d01ec6942c178af1316b3b359aa34dfae3
Oleg Kolosov <oleg@MacBook-Pro-Oleg.local>
2024-08-12 13:08:28 +0300

    lalala

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,6 +9,9 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 
+
+
+
 export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
@@ -31,11 +34,11 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   try {
     const data = await sql<LatestInvoiceRaw>`
-      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
-      FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
-      ORDER BY invoices.date DESC
-      LIMIT 5`;
+    SELECT invoices.amount, customers.name, customers.image_url, customers.email
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    ORDER BY invoices.date DESC
+    LIMIT 5`;
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
